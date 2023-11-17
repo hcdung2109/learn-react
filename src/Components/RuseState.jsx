3b9814e9c37f8025cdea50f2d781ebbfc1019250
DESKTOP-G94FLTS\hcdung <hcdung@vnpt.vn>
2023-11-17 17:12:20 +0700

    update

--- src/Components/RuseState.jsx
+++ src/Components/RuseState.jsx
@@ -5,8 +5,25 @@ const RuseState = () => {
     const [count, setCount] = useState(0)
 
     const handleClick = function () {
-        setCount(count + 1)
+        //setCount(count + 1)
+
+        setCount((prevState => prevState + 1))
+    };
+
+    const [infoUser, setInfoUser] = useState({
+        name: 'Hoang Cong Dung',
+        age: 32
+    });
+
+    const handleUpdate = () => {
+        setInfoUser({
+            ...infoUser,
+            address: "Ha Noi"
+        })
     }
+
+
+    console.log('re-render');
 
 
     return (
@@ -15,6 +32,10 @@ const RuseState = () => {
             <p>{name}</p>
             <p>Count: {count}</p>
             <button onClick={handleClick}>Click</button>
+            <h3>VD 2 :</h3>
+            <p>{ JSON.stringify(infoUser) }</p>
+            <button onClick={handleUpdate}>Update</button>
+            <h3>---- END useState</h3>
         </div>
     );
 };
